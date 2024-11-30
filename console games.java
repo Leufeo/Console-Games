b288801fd61d988170cdbe1bd7e83789388a8237
Leon Broda <158483555+Leufeo@users.noreply.github.com>
2024-11-30 15:45:55 +0100

    description console games.java

--- console games.java
+++ console games.java
@@ -1,3 +1,15 @@
+/* CONSOLE GAMES by Leon Broda
+
+To choose a field on a board type in the row number first and the column number second.
+You do not need any charcter or blank space in between.
+examples: "12" referes to the second field of the first line.
+          "31" referes to the first field of the third line.
+
+If in tic tac toe the gravity is activated type only one number.
+For checkers you additionally need to choose a direction.
+Add r for right or l for left.
+To move a king u (up) or d (down) needs to be the last character of your input.*/
+
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Objects;
